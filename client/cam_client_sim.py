ed312069da8b0752252972d1517e5ceff5d621c7
Miko Bautista <mikobautista@dhcp-18-40-24-135.dyn.mit.edu>
2018-09-17 11:15:40 -0400

    animations for demo

--- client/cam_client_sim.py
+++ client/cam_client_sim.py
@@ -9,26 +9,24 @@ sock_client = SocketIO('localhost', 3000, LoggingNamespace)
 i = 0
 
 while True:
-    if i < 10:
+    
+    arr = [10,20,30,40,50]
+    x_1, x_2, x_3 = arr[i], arr[4-i], 50
+    y_1, y_2, y_3 = arr[i], arr[4-i], 30
+    hand1_x_1, hand1_y_1 = x_1 - 5, y_1 + 5
+    hand2_x_1, hand2_y_1 = x_1 + 30, y_1 + 5
+    
+    hand1_x_2, hand1_y_2 = x_2 - 5, y_2 + 5
+    hand2_x_2, hand2_y_2 = x_2 + 30, y_2 + 5
 
-        x_1, x_2 = (25*math.sin(i/10.) + 50), (10*math.sin(i/89.) + 20)
-        y_1, y_2 = (10*math.cos(i/80.) + 10), (5*math.sin(i/70.) + 20)
-
-        sock_client.emit('messages',
-                         json.dumps([
-                            {'person_id': 'Billy', 'x': x_1, 'y': y_1},
-                            {'person_id': 'Tommy', 'x': x_2, 'y': y_2}
-                         ]))
-    else:
-        x_1 = (25*math.sin(i/100.) + 75)
-        y_1 = (10*math.cos(i/80.) + 8)
-        sock_client.emit('messages',
-                         json.dumps([
-                            {'person_id': 'Billy', 'x': x_1, 'y': y_1},
-                         ]))
-
+    sock_client.emit('messages',
+                     json.dumps([
+                        {'person_id': 'Billy', 'x': x_1, 'y': y_1, 'hand1_x': hand1_x_1, 'hand1_y': hand1_y_1, 'hand2_x': hand2_x_1, 'hand2_y': hand2_y_1},
+                        {'person_id': 'Tommy', 'x': x_2, 'y': y_2, 'hand1_x': hand1_x_2, 'hand1_y': hand1_y_2, 'hand2_x': hand2_x_2, 'hand2_y': hand2_y_2},
+                        {'person_id': 'Eric', 'x': x_3, 'y': y_3}
+                     ]))
     i += 1
-    if i > 40:
+    if i > 4:
         i = 0
 
-    time.sleep(0.2 + 0.2*random.random())
+    time.sleep(1)
